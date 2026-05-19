362583d7c1a9518b0ec51961d4d6eed32ef6bc88
Juan Arteaga <juan.arteaga.lasluisa@udla.edu.ec>
2026-05-19 12:02:26 -0500

    fase 2 validacion

--- main.c
+++ main.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 #include "funciones.h"
-
+ 
 int main()
 {
     int n;
-
+ 
     do
     {
         printf("Ingrese numero de estudiantes (1-100): ");
         scanf("%d", &n);
-
+ 
         if(n <= 0 || n > 100)
         {
             printf("ERROR -> Numero invalido\n");
         }
-
+ 
     }while(n <= 0 || n > 100);
-
+ 
     float notas[100][MATERIAS];
-
+ 
     ingresar(notas, n);
-
+ 
     mostrar(notas, n);
-
+ 
     return 0;
 }
